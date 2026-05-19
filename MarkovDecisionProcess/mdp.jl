a2dea2286a4dd4cad1644c729a9febdfcb8fd562
Ben Hwang <bdhwang@ucsd.edu>
2026-05-18 17:15:11 -0700

    mdp notes

---
+++ MarkovDecisionProcess/mdp.jl
@@ -0,0 +1,14 @@
+###
+# Add state, action, environmnet, reward 
+#
+# state: locally cleared generation and price, also capacity and other internal variables
+# action: bid
+# environment: market clearing <- lower level opt, and competitor's bids
+# reward: profit 
+# 		eventually, this can be calculated by a separate
+# 		tracking problem, but for now lets just assume we have
+# 		perfect tracking capability
+
+
+
+
