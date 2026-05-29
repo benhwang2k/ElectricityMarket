4a4e5b0e1e6816dfc79e9ec5de4cbb61ef346da4
Ben Hwang <bdhwang@ucsd.edu>
2026-05-29 14:04:53 -0700

    state trans

---
+++ MarkovDecisionProcess/marketclear.jl
@@ -0,0 +1,108 @@
+module Market
+using JuMP, Gurobi, CSV, DataFrames, Statistics
+export PowerPlant, solve_market, get_profit
+
+mutable struct PowerPlant
+    name::String
+    capacity::Float64
+    min_output::Float64
+    variable_cost::Float64
+    startup_cost::Float64
+    ramp_up::Float64
+    ramp_down::Float64
+    init_commit::Int
+    init_gen::Float64
+    is_strategic::Bool
+end
+
+function solve_market(plants, demands, k)
+    model = Model(Gurobi.Optimizer)
+    set_silent(model)
+
+    I = eachindex(plants)
+    H = eachindex(demands)
+
+    #setting up variables g[i, h], u[i, h], y[i, h] (u, y are binary)
+    @variable(model, generation[i in I, h in H] >= 0)
+    @variable(model, commitment[i in I, h in H], Bin)
+    @variable(model, startup[i in I, h in H], Bin)
+
+    #minimize the cost, considering startup costs and strategic bidders
+    @expression(model, startup_cost,
+        sum(startup[i, h] * plants[i].startup_cost for i in I for h in H))
+    @expression(model, variable_cost,
+	sum(generation[i, h] * (plants[i].is_strategic ? k * plants[i].variable_cost : 
+        plants[i].variable_cost) for i in I for h in H))
+    @objective(model, Min, startup_cost + variable_cost)
+
+    #upper bound, generation must be less than capacity
+    @constraint(model, [i in I, h in H], generation[i, h] <= plants[i].capacity * commitment[i, h])
+
+    #lower bound, if generator is on must produce min output
+    @constraint(model, [i in I, h in H], generation[i, h] >= plants[i].min_output * commitment[i, h])
+
+
+    #making it so startup cost is 1 when u goes from 0 to 1
+    @constraint(model, [i in I, h in H[2:end]], startup[i, h] >= commitment[i, h] - commitment[i, h-1])
+
+    #maybe include the startup edge cases, where ramp_up and ramp_down don't apply in that window, allowing for
+    #generators to actaully start up if the min output is greater than their ramp up
+
+    #ramping up and down constraints on generation
+    @constraint(model, [i in I, h in H[2:end]], generation[i, h] - generation[i, h-1] <= plants[i].ramp_up)
+    @constraint(model, [i in I, h in H[2:end]], generation[i, h-1] - generation[i, h] <= plants[i].ramp_down)
+
+    #initial condition constraints (so we can start from any combination of market horizons)
+    @constraint(model, [i in I], generation[i, 1] - plants[i].init_gen <= plants[i].ramp_up)
+    @constraint(model, [i in I], plants[i].init_gen - generation[i, 1] <= plants[i].ramp_down)
+    @constraint(model, [i in I], startup[i, 1] >= commitment[i,1] -  plants[i].init_commit)
+
+
+    #fufilling demand
+    @constraint(model, demand_balance[h in H], sum(generation[i, h] for i in I) == demands[h])
+
+    optimize!(model)
+    
+    gen_values = [value(generation[i, h]) for i in I, h in H]
+    startup_values = [value(startup[i,h]) for i in I, h in H]
+    
+    #fixing commitment values and then rerunning the program
+    undo = fix_discrete_variables(model);
+
+    optimize!(model)
+    price = dual.(demand_balance)
+
+    undo()
+
+    profit = 0.0
+    strategic_generation = 0.0
+
+    for i in I
+        for h in H
+            if plants[i].is_strategic
+                strategic_generation += gen_values[i, h]
+                #calculating profits from dual of demand constraint
+                profit += (price[h] - plants[i].variable_cost) * gen_values[i, h] - 
+                    startup_values[i, h] * plants[i].startup_cost
+            end
+        end
+    end
+    return gen_values, price, profit, strategic_generation
+end
+
+function get_profit(plant, gen_values, startup_values)
+    profit = 0.0
+    strategic_generation = 0.0
+
+    for h in H
+	if plant.is_strategic
+	    strategic_generation += gen_values[i, h]
+	    #calculating profits from dual of demand constraint
+	    profit += (price[h] - plants[i].variable_cost) * gen_values[i, h] - 
+		startup_values[i, h] * plants[i].startup_cost
+	end
+    end
+    return profit
+end
+
+end
--- MarkovDecisionProcess/mdp.jl
+++ MarkovDecisionProcess/mdp.jl
@@ -1,3 +1,6 @@
+using .Market
+using CSV, DataFrames
+using Random
 ###
 # Add state, action, environmnet, reward 
 #
@@ -8,6 +11,146 @@
 # 		eventually, this can be calculated by a separate
 # 		tracking problem, but for now lets just assume we have
 # 		perfect tracking capability
+
+
+# Each real time market is calculated for a number of periods T
+T = 1
+
+
+# Cleared generation for a horizon T
+g = [0.0]*T
+# Price at each time
+l = [0.0]*T
+# commitment for each time
+u = [false]*T
+
+
+mutable struct State
+  gen::Matrix{Float64}
+  price::Vector{Float64}
+end
+
+
+struct Bid
+  competition::Vector{Float64}
+end
+
+
+plants_df = CSV.read("MarkovDecisionProcess/data/plants.csv", DataFrame)
+plants = Market.PowerPlant[]
+for i in eachindex(plants_df.name)
+    push!(plants, Market.PowerPlant(
+        plants_df.name[i],
+        plants_df.capacity[i],
+        plants_df.min_output[i],
+        plants_df.variable_cost[i],
+        plants_df.startup_cost[i],
+        plants_df.ramp_up[i],
+        plants_df.ramp_down[i],
+        plants_df.init_commit[i],
+        plants_df.init_gen[i],
+        Bool(plants_df.is_strategic[i])
+    ))
+end
+
+
+demands_df = CSV.read("MarketClearing/data/demand.csv", DataFrame)
+
+
+function next_state(plants, state, new_demand)
+  if !(is_demand_feasible(plants, new_demand))
+    print("demand not feasible")
+    return state
+  end
+  # capactiy of next day = cap of prev - gen + solar
+  k = 1
+  
+  # solve
+  g, price, profit, one_strategic_gen = solve_market(plants, new_demand, k)
+  next_state = State(g, price)  
+
+  # plant controllable capacity (ie gas generation)
+  # plus a stochastic renewable resource
+  # storage term
+  gas_gen = [200, 300, 100, 250]
+  renew_gen = [100, 100, 100, 100]
+  battery_cap = [100, 100, 100, 100]
+  for i in 1:4 
+    plants[i].capacity = max(min(battery_cap[i], plants[i].capacity - gas_gen[i] -state.gen[i]),0) + renew_gen[i]*rand() + gas_gen[i] 
+    if state.gen[i] > 0
+      plants[i].init_commit = 1 
+    else
+      plants[i].init_commit = 0
+    end
+    plants[i].init_gen = state.gen[i] 
+  end
+  return next_state
+end
+
+
+function is_demand_feasible(plants, demand)
+  downcapacity = 0
+  capacity = 0
+  for p in plants
+    upc = min(p.init_gen + p.ramp_up, p.capacity)
+    downc = max(p.init_gen - p.ramp_down, p.min_output)
+    if p.init_gen < p.ramp_down
+      downc = 0
+    end
+    capacity += upc
+    downcapacity += downc
+  end
+  return downcapacity <= demand <= capacity
+end
+
+
+function print_cap(plants)
+  println()
+  println()
+  println("capacities") 
+  println([p.capacity for p in plants])
+  println()
+  println()
+end
+
+
+function simulate()
+  plants = Market.PowerPlant[]
+  for i in eachindex(plants_df.name)
+      push!(plants, Market.PowerPlant(
+	  plants_df.name[i],
+	  plants_df.capacity[i],
+	  plants_df.min_output[i],
+	  plants_df.variable_cost[i],
+	  plants_df.startup_cost[i],
+	  plants_df.ramp_up[i],
+	  plants_df.ramp_down[i],
+	  plants_df.init_commit[i],
+	  plants_df.init_gen[i],
+	  Bool(plants_df.is_strategic[i])
+      ))
+  end
+
+  # constant load
+  demand = 300 
+  # solve
+  g, price, profit, one_strategic_gen = solve_market(plants, demand, 1)
+  s = State(g, price)  
+  state_array = [s]
+  for t in 1:5
+    if t > 2
+      demand = 400
+    end
+    print("t = ")
+    print(t)
+    println()
+    s = next_state(plants, s, demand)
+    push!(state_array, s)
+    print_cap(plants)
+  end
+  return state_array, plants
+end
+
 
 
 
